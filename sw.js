65ae81330075ba283229f98cc678f5cc13df60d2
sherifmorshed <74205549+sherifmorshed@users.noreply.github.com>
2026-08-15 20:12:26 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  requests are NEVER cached to prevent stale data.
 // ══════════════════════════════════════════
 
-const CACHE_NAME = 'accounting-prod-v45';
+const CACHE_NAME = 'accounting-prod-v52';
 
 // Only cache static files that don't change between sessions
 const STATIC_ASSETS = [
